366882d9f806c2e94da5c33a72d1a3d5ebdc2c67
letdayy <lcdayane49@gmail.com>
2023-05-10 16:26:07 -0300

    feita a estilização dos cards

--- src/app/features/home/components/course-list/course-list.component.ts
+++ src/app/features/home/components/course-list/course-list.component.ts
@@ -11,18 +11,21 @@ export class CourseListComponent implements OnInit {
   courses: Array<Course> = [
     {
       id: 1,
+      image: 'https://t42748.vteximg.com.br/arquivos/ids/156666-1920-1280/programacao-de-computadores.jpg?v=637581756026170000',
       description: 'Introdução à programação de computadores',
       price: 27.90
     },
     {
       id: 2,
-      description: 'Introdução à programação de computadores',
-      price: 27.90
+      image: 'https://img-c.udemycdn.com/course/750x422/1927044_a831.jpg',
+      description: 'Curso de Python 3 do Básico Ao Avançado',
+      price: 39.90
     },
     {
       id: 3,
-      description: 'Introdução à programação de computadores',
-      price: 27.90
+      image: 'https://sujeitoprogramador.com/wp-content/uploads/2019/01/sc.png',
+      description: 'React Native na Prática',
+      price: 39.90
     },
   ];
 
--- src/app/features/home/models/course.model.ts
+++ src/app/features/home/models/course.model.ts
@@ -1,5 +1,6 @@
 export interface Course {
     id: number;
+    image: string;
     description: string;
     price: number;
 }
